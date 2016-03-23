27a0c86451d904fc1562b9706e5e1a62119b9349
Nate Merrill <merrill.nathaniel@epa.gov>
2016-03-23 16:26:33 -0400

    For amy notes

--- Estuary_inandout2/Capegroundinadout2.m
+++ Estuary_inandout2/Capegroundinadout2.m
@@ -1,50 +1,58 @@
+
 %% This is the start of the groundwater transit time problem
+
+%Author: Nate Merrill
+
+%this file has the input paramters and calls the function files estuaryi2
+%which is the model of the estuary through time, which uses a file dettman
+%(name of a scientist (Ed Dettman) here whose "model" I adapted  for estuary N stocks)
+
 clear all
 
-%parameters
-n=30                     % years
+%parameters- these are all placeholders until we parameterize it for an
+%estuary system
+
+n=20                   % years
 s=3                    % sources
-k=.02                     % annual attenuation rate 
-r=.1                    % discount rate
-
-
-R=    .3            %net annual removel rate
+k=.1                   % annual attenuation rate 
+r=.05                  % discount rate
+B=100                  % background/ non controllable sources
+R=   .5                % net N caryover from year before
 IN0=100
 %tau=randi([0 100],[s,1]) % distribution of transit times
-tau= [3, 2, 1]'
-taum=max(tau)
-c=zeros(s,n+taum)         % matrix of sources
+tau= [5, 3, 1]'        %transit time for each source
+taum=max(tau)          %this is used to deal with load before choice years
+c=zeros(s,n+taum)      % matrix of sources
 
 c(1,:)=10                % yearly N input rate
 c(2,:)=10
 c(3,:)=10
 
-cost_s=1000
-cost_in=1000
-%balance=.5
-control_s=ones(s+1,n)
-control_s(:,:)=.1
+cost_s=1000             % cost parameter for source control
+cost_in=2000            % cost parameter for in-estuary control
 
-[npv damage IN net] = estuaryi2(control_s,c,taum,n,k,tau,s,r,cost_s,cost_in,R,IN0);
+control_s=ones(s+1,n);  % each cell represents the % of total load left after treatment of each source
+control_s(:,:)=.1;      % each cell is the in % in estuary N left in estuary after treatment
 
-mnpv=@(control_s) estuaryi2(control_s,c,taum,n,k,tau,s,r,cost_s,cost_in,R,IN0);
+% This part gets the optimal control back and other outputs
+[npv damage IN net] = estuaryi2(control_s,c,taum,n,k,tau,s,r,cost_s,cost_in,R,IN0,B);
 
-%options=optimset('Display','final','PlotFcns',@optimplotfval,'MaxFunEvals',10e10,'Algorithm','sqp');
-lb=zeros(size(control_s))
-ub=zeros(size(control_s))
-ub(:,:)=1
-%[x,fval,exitflag] =fmincon(mnpv,control_s,[],[],[],[],lb,ub,[],options);
-%V
+mnpv=@(control_s) estuaryi2(control_s,c,taum,n,k,tau,s,r,cost_s,cost_in,R,IN0,B);
 
-options=optimset('Display','final','Algorithm','sqp','MaxFunEvals', 10e5,'TolX',10e-5);
-lb=zeros(size(control_s))
-ub=ones(size(control_s))
+options=optimset('Display','final','Algorithm','sqp','MaxFunEvals', 10e5,'TolX',10e-5,'PlotFcns',@optimplotfval);
+lb=zeros(size(control_s));
+ub=ones(size(control_s));
 
 [x,fval,exitflag] =fmincon(mnpv,control_s,[],[],[],[],lb,ub,[],options);
 
-[npvop damageop INop netop]=estuaryi2(control_s,c,taum,n,k,tau,s,r,cost_s,cost_in,R,IN0)
-;
+[npvop damageop INop netop]=estuaryi2(x,c,taum,n,k,tau,s,r,cost_s,cost_in,R,IN0,B);
 
+
+%plots control and in estuary N over time
+subplot(2,1,1)
 plot(x(:,1+taum:end)')
+subplot(2,1,2)
+plot(INop(:,1+taum:end))
+
 
 %plot(damage(1+taum:end))
--- Estuary_inandout2/estuaryi2.m
+++ Estuary_inandout2/estuaryi2.m
@@ -1,31 +1,53 @@
-function [npv damage IN net] = estuaryi2(control_s,c,taum,n,k,tau,s,r,cost_s,cost_in,R,IN0)
+function [npv damage IN net] = estuaryi2(control_s,c,taum,n,k,tau,s,r,cost_s,cost_in,R,IN0,B)
+% esturaryi2  - Simulates loading and estuary N stock
+% control_s - control matrix
+% c - source loadings uncontrolled
+% taum - the max of the delay times (this is to deal with the pre-control
+% time
+% n - number of years
+% k - % N removed per year in transit
+% tau - matrix of time delay for sources
+% s- number of sources
+% cost_s- cost parameter for sources
+% cost_in - cost parameter for in-estuary treatment
+% R - net N caryover from year before
+% IN0 - starting N stock
+% B - background annual loading from non-controllable sources
 
 c2=zeros(1,s)';
 cost=zeros(1,s)';
 control_s1=[ones(s+1,taum) control_s];
-TC=sum(c,1);
+TC=sum(c,1); 
 IN=zeros(1,n);
-IN(:,:)=IN0;
+IN(:,1)=IN0;
 
-for i=taum+1:taum+n;
+for i=2:taum; %creates pre-control N stock
+    IN(:,i)=dettman(TC(i),R,IN(:,i-1),B); 
+end
+
+for i=taum+1:taum+n; % this loop runs the control through time
  
-    for ii=1:s;
+    for ii=1:s; %calculates total load depending on year with delay time tau
     c2(ii,1)=(control_s1(ii,i-tau(ii))*c(ii,i-tau(ii)))*exp(-k*tau(ii));
     end
     
-    TC1(i)=sum(sum( c2 ));
-    IN(i)=dettman(TC1(i),R,IN(i-1));
-    TC(i)= IN(i).*control_s1(s+1,i);
+    tc1=sum(sum(c2)); % sums total load
     
-cost(i)=sum(cost_s.*(1./control_s1(1:s,i)-1).*c(1:s,i)) + (cost_in.*(1./control_s1(s+1,i)-1)).*IN(i);
+    IN(i)=dettman(tc1,R,TC(i-1),B); % calculates N stock for year i
 
-damage(i)=(100*TC(i)^2);
+    TC(i)= IN(i).*control_s1(s+1,i); % calculates N stock for year i after in estuary treatment
+    
+    
+cost(i)=sum(cost_s.*(1./control_s1(1:s,i)-1).*c(1:s,i)) + (cost_in.*(1./control_s1(s+1,i)-1)).*IN(i); %creates cost of year i based on choices in control matrix
 
-net(i)=(damage(i)+cost(i))*exp(-r*i);
+damage(i)=(100*TC(i)^2); %damages of N stock in estuary
+
+net(i)=(damage(i)+cost(i))*exp(-r*i); %  damages+costs
 
 end
 
-
+%This part was when I was using fminsearch, needed to penalize non-feasible
+%options
 if control_s1(:,:)>0 & control_s1(:,:)<=1;
 npv=sum(net);
 
